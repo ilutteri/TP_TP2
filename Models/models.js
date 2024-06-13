713a6e55571c5b92356782ef74a290bfc7676d85
Ignacio Lutteri <ignaciolutteri@gmail.com>
2024-06-13 18:27:17 -0300

    Cambio de relaciones entre Modelos

--- Models/models.js
+++ Models/models.js
@@ -3,11 +3,26 @@ import Tablero from "./Tablero.js";
 import Pokemon from "./Pokemon.js";
 
 
-Tablero.hasMany(User, {foreignKey:"tableroId"})
-User.belongsTo(Tablero, {foreignKey:"tableroId"})
+User.hasOne(Tablero, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+Tablero.belongsTo(User, { foreignKey: "userId" });
 
-Tablero.hasMany(Pokemon, {foreignKey:"tableroId"})
-Pokemon.belongsTo(Tablero, {foreignKey:"tableroId"})
+
+Tablero.hasMany(Pokemon, {
+  foreignKey: "tableroId",
+  onDelete: "CASCADE",
+  hooks: true,
+});
+Pokemon.belongsTo(Tablero, { foreignKey: "tableroId" });
 
 
-export {Tablero, User, Pokemon}
+Tablero.addHook("beforeValidate", async (tablero, options) => {
+  const pokemonCount = await Pokemon.count({ where: { tableroId: tablero.id } });
+  if (pokemonCount > 6) {
+    throw new Error("Un tablero no puede tener mas de 6 Pokémon.");
+  }
+});
+
+export { User, Tablero, Pokemon };
