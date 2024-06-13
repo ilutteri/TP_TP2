8f0ad8d777538168c2f0dbe97d4afc0a20cc1d9e
Ignacio Lutteri <ignaciolutteri@gmail.com>
2024-06-13 18:03:04 -0300

    relacion entre tablas

--- Models/models.js
+++ Models/models.js
@@ -2,10 +2,12 @@ import User from "./User.js";
 import Tablero from "./Tablero.js";
 import Pokemon from "./Pokemon.js";
 
+
 Tablero.hasMany(User, {foreignKey:"tableroId"})
 User.belongsTo(Tablero, {foreignKey:"tableroId"})
 
 Tablero.hasMany(Pokemon, {foreignKey:"tableroId"})
 Pokemon.belongsTo(Tablero, {foreignKey:"tableroId"})
 
-export {User, Tablero, Pokemon}
+
+export {Tablero, User}
