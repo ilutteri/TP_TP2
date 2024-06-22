6e46d9f96fe50eac3b9d1f277a417f73daa84954
Ignacio Lutteri <ignaciolutteri@gmail.com>
2024-06-21 21:09:54 -0300

    emprolijamiento de codigo

--- Models/Tablero.js
+++ Models/Tablero.js
@@ -1,12 +1,10 @@
 import { DataTypes, Model } from "sequelize";
 import connection from "../connection/connection.js";
 
-class Tablero extends Model {}
+class Tablero extends Model { }
 
 Tablero.init(
-  {
-
-  },
+  {},
   {
     sequelize: connection,
     modelName: "Tablero",
